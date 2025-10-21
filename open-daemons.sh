8561927873f1c28821426c744b498c2ee189f722
kaungmyatkyawtiti <eucalyptustiti2003@gmail.com>
2025-10-21 17:15:02 +0630

    chore(open-daemons): init shell script to github

---
+++ open-daemons.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo systemctl start mongod
+echo "started MongoDB..."
+
+sudo systemctl start mariadb
+echo "started MariaDB..."
+
+sudo systemctl start php-fpm
+echo "started PHP-Fpm..."
+
+sudo systemctl start nginx
+echo "started Nginx..."
